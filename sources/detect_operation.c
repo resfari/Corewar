7d48f90e6d93f1ac78bff975a22184d8111520f4
Lieutenant Georgia <lgeorgia@at-q4.21-school.ru>
2019-11-14 16:20:49 +0300

    modified file

--- sources/detect_operation.c
+++ sources/detect_operation.c
@@ -6,7 +6,7 @@
 /*   By: lgeorgia <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/06 21:39:09 by lgeorgia          #+#    #+#             */
-/*   Updated: 2019/11/11 16:21:23 by lgeorgia         ###   ########.fr       */
+/*   Updated: 2019/11/14 16:07:51 by lgeorgia         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,5 +20,9 @@ void	ft_detect_operation(int op_num, t_war *war, t_crg *crg)
 		// printf("pos = %d\n",crg->pos);
 
 		ft_live(war, crg);
+	}
+	if (op_num == 2)
+	{
+		ft_ld(war, crg);
 	}
 }
