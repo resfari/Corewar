d472dc33cd4355e0dfbec71a04953acc7a37955b
Pearse Nita <pnita@at-q3.21-school.ru>
2019-11-16 20:22:56 +0300

    sti

--- sources/get_arg.c
+++ sources/get_arg.c
@@ -19,15 +19,12 @@ int get_arg_reg(t_war *war, int pos)
 
 int get_arg_dir(t_war *war, int pos, int size)
 {
-	if (size == 4)
-	{
-		return ((war->arena[GG(pos)].code) << 24 | (war->arena[GG(pos + 1)].code) << 16 |
-			(war->arena[GG(pos + 2)].code) << 8 | (war->arena[GG(pos + 3)].code));
-	}
-	else if (size == 2)
+	if (size == 2)
 	{
 		return ((war->arena[GG(pos)].code) << 8 | (war->arena[GG(pos + 1)].code));
 	}
+	return ((war->arena[GG(pos)].code) << 24 | (war->arena[GG(pos + 1)].code) << 16 |
+		(war->arena[GG(pos + 2)].code) << 8 | (war->arena[GG(pos + 3)].code));
 }
 
 int get_arg_ind(t_war *war, int pos)
