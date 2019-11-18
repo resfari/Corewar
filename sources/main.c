a8762d3bac9ddfdb499a4dd3e88c345bffa0325c
Lieutenant Georgia <lgeorgia@at-q4.21-school.ru>
2019-11-18 22:19:14 +0300

    some fix

--- sources/main.c
+++ sources/main.c
@@ -6,7 +6,7 @@
 /*   By: lgeorgia <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/02 22:09:53 by lgeorgia          #+#    #+#             */
-/*   Updated: 2019/11/18 19:45:30 by lgeorgia         ###   ########.fr       */
+/*   Updated: 2019/11/18 22:16:57 by lgeorgia         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,7 +28,7 @@ int		main(int argc, char **argv)
 
 	// ft_print_arena(war);
 
-	// ft_init_ncurses();
+	ft_init_ncurses();
 	ft_play_game(war);
 	
 
