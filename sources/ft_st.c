5d38faecbfe9f35de41219eb9e178544faeb10e3
Lieutenant Georgia <lgeorgia@at-q4.21-school.ru>
2019-11-19 20:24:53 +0300

    Bug fix

--- sources/ft_st.c
+++ sources/ft_st.c
@@ -6,7 +6,7 @@
 /*   By: lgeorgia <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/15 20:26:20 by pnita             #+#    #+#             */
-/*   Updated: 2019/11/19 18:16:18 by lgeorgia         ###   ########.fr       */
+/*   Updated: 2019/11/19 20:23:00 by lgeorgia         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,11 +31,11 @@ void	ft_st_reg(t_war *war, t_crg *crg)
 	}
 }
 
-int		ft_st_ind_arg(t_war *war, int pos)
+static int		ft_st_ind_arg(t_war *war, int pos)
 {
 	unsigned short ind;
 
-	ind = ((255 & (war->arena[pos].code) << 8) | (255 & war->arena[pos].code));
+	ind = ((255 & (war->arena[GG(pos)].code) << 8) | (255 & war->arena[GG(pos + 1)].code));
 	return((short)ind);
 }
 
@@ -43,15 +43,17 @@ void	ft_st_ind(t_war *war, t_crg *crg)
 {
 	int pos;
 	int reg;
-	int	ind;
+	short	ind;
 	int i;
 
 	pos = crg->pos + 2;
+	ind = 0;
 
 	reg = war->arena[GG(pos)].code;
-	// printf("\nreg = %d\n", reg);
-	ind = ft_st_ind_arg(war, pos + 1);
-	pos = crg->pos + ind % IDX_MOD;
+	ind = (ft_st_ind_arg(war, crg->pos + 3));
+	ind = ind % IDX_MOD;
+	printf("\nreg = %d   IND = %d  posDIR = %d\n", reg, ind, pos + 1);
+	pos = crg->pos + ind;
 	if (reg >= 1 && reg <= 16)
 	{
 		i = 0;
@@ -63,6 +65,9 @@ void	ft_st_ind(t_war *war, t_crg *crg)
 			i++;
 		}
 	}
+	// ft_print_1(war);
+	exit(1);
+	// exit(1);
 }
 
 void	ft_st(t_war *war, t_crg *crg)
